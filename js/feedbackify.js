b91dec5ebbceac5ca4d0b059f9ac2b363341c1bf
Cornel Andreev <hugronaphor@gmail.com>
2015-09-27 14:50:17 +0100

    Fix JS.

--- js/feedbackify.js
+++ js/feedbackify.js
@@ -3,27 +3,26 @@
  * JavaScript for the Feedbackify module.
  */
 
-(function($) {
+"use strict";
 
-  "use strict";
+/**
+ * Add the Feedbackify script async.
+ */
+var feedbackifyId = drupalSettings.feedbackify.feedbackifyId,
+  feedbackifyPos = drupalSettings.feedbackify.feedbackifyPos,
+  feedbackifyColor = drupalSettings.feedbackify.feedbackifyColor;
 
-  /**
-   * Add the Feedbackify script async.
-   */
-
-  var feedbackifyId = drupalSettings.feedbackify.feedbackifyId,
-          feedbackifyPos = drupalSettings.feedbackify.feedbackifyPos,
-          feedbackifyColor = drupalSettings.feedbackify.feedbackifyColor;
-
-  var fby = fby || [];
-  fby.push(['showTab', {id: feedbackifyId, position: feedbackifyPos, color: feedbackifyColor}]);
-  (function() {
-    var f = document.createElement('script');
-    f.type = 'text/javascript';
-    f.async = true;
-    f.src = '//cdn.feedbackify.com/f.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(f, s);
-  })();
-
-})(jQuery);
+var fby = fby || [];
+fby.push(['showTab', {
+  id: feedbackifyId,
+  position: feedbackifyPos,
+  color: feedbackifyColor
+}]);
+(function () {
+  var f = document.createElement('script');
+  f.type = 'text/javascript';
+  f.async = true;
+  f.src = '//cdn.feedbackify.com/f.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(f, s);
+})();
